0d626891d0f7af2e9a340744055c0fac3a8d88d3
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-19 23:50:24 +0100

    puppet create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Define a file resource type
-file {'/school':
-  'path'    => '/tmp/school',
-  'mode'    => '0744',
-  'owner'   => 'www-data',
-  'group'   => 'www-data',
-  'content' => 'I love Puppet'
+file {"/tmp/school":
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
