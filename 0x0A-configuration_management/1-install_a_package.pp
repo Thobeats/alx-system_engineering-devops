319253080f6074c21c22a4a9974d97c76753c047
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-20 14:55:41 +0100

    install flask package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,10 @@
 # Install flask using pip3
-class flask {
-
-  package {'werkzeug':
-    ensure   => '2.1.1',
-    provider => 'pip3'
-  }
-
-  package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
-  }
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
 
-include flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
