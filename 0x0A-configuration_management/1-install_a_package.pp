6f29296291beb1ad9ab3e9fdd55d858bf187e1fb
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-20 05:02:22 +0100

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#install a flask from pip3
-exec { 'install flask':
-  command =>  'pip3 install flask==2.1.0',
-  path    =>  '/usr/bin/',
-  unless  =>  'pip3 list | grep flask',
+# Install flask using pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
