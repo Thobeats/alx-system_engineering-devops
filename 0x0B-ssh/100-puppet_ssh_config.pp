24c47a4eee57a1ad943eeaa18add35841c5cd856
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-22 00:00:21 +0100

    puppet ssh config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# set up your client SSH configuration file
+# so that you can connect to a server without
+# typing a password.
+file {'etc/ssh/school':
+  content     => 'PasswordAuthentication no
+  Identity ~/.ssh/school'
+}
