08e8a322a4ee061b3134f6ca2ac8eed91b8597c3
Iyanu Dayisi <tobiy23@gmail.com>
2024-05-21 00:59:09 +0100

    gather data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,11 +16,12 @@ user = user_req.json()
 api_req = requests.get(url)
 all_tasks = api_req.json()
 total = len(all_tasks)
-completed_tasks = list(filter(lambda task: task['completed'] == True, all_tasks))
+completed_tasks = list(filter(lambda task: task['completed'] is True,
+                              all_tasks))
 completed = len(completed_tasks)
-response = "Employee {} is done with tasks({}/{}):\n".format(user['name'], completed, total)
+response = "Employee {} is done with tasks({}/{}):\n".format(user['name'],
+                                                             completed,
+                                                             total)
 for task in completed_tasks:
     response += "   {}\n".format(task['title'])
 print(response)
-
-
