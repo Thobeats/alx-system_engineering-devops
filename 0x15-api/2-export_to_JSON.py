829da1cf6d546b26b71f351bd18a9bd0ca56f388
Iyanu Dayisi <tobiy23@gmail.com>
2024-05-21 01:43:47 +0100

    export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Write a Python script that, using this REST API,
+for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    employee_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+    user_url = "{}/users/{}".format(url, employee_id)
+    todo = "{}/users/{}/todos".format(url, employee_id)
+    user_req = requests.get(user_url)
+    user = user_req.json()
+    api_req = requests.get(todo)
+    all_tasks = api_req.json()
+    filename = "{}.json".format(employee_id)
+    with open(filename, 'w', newline='') as file:
+       json.dump({employee_id: [{
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": user['username']
+        } for task in all_tasks]}, file)
+        
