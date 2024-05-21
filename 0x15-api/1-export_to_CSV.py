94b73c2ae8bab903dca0fc66bedc8d607e22e9e3
Iyanu Dayisi <tobiy23@gmail.com>
2024-05-21 01:30:39 +0100

    export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Write a Python script that, using this REST API,
+for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    employee_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+    user_url = "{}/users/{}".format(url, employee_id)
+    todo = "{}/users/{}/todos".format(url, employee_id)
+    user_req = requests.get(user_url)
+    user = user_req.json()
+    api_req = requests.get(todo)
+    all_tasks = api_req.json()
+    filename = "{}.csv".format(employee_id)
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [employee_id, user['name'], task['completed'], task['title']]
+        ) for task in all_tasks]
