5af0f92819aab806ee8c1b32ae49931d8dbe8515
Iyanu Dayisi <tobiy23@gmail.com>
2024-06-04 23:03:10 +0100

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Write a function that queries the
+Reddit API and prints the titles
+of the first 10 hot posts listed
+for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Number of Subreddit Subscribers
+    Keyword arguments:
+    subreddit -- the subreddit to check e.g programming.
+    Return: the number of subscribers or 0 if the subreddit doesn't exist
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"user-agent": "user"}
+    reddit_request = requests.get(url,
+                                  headers=headers,
+                                  allow_redirects=False)
+    if (reddit_request.status_code != 200):
+        print('None')
+        return
+    reddit_json = reddit_request.json()
+    for reddit in reddit_json['data']['children']:
+        print(reddit.get('data').get('title'))
