ae6c02f7b81e24c0df6198521caaeb589d1b5249
Iyanu Dayisi <tobiy23@gmail.com>
2024-06-04 22:08:59 +0100

    get all subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,10 +2,6 @@
 """
 Write a function that queries the Reddit API
 and returns the number of subscribers
-(not active users, total subscribers)
-for a given subreddit.
-If an invalid subreddit is given,
-the function should return 0.
 """
 import requests
 
