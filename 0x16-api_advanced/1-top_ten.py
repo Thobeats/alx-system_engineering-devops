db510dc8761798eaa31aed1b3d38d70ab99bb104
Iyanu Dayisi <tobiy23@gmail.com>
2024-06-04 23:21:55 +0100

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
 """
-defines function to query the Reddit API for
-prints titles of the first 10 hot posts
+Write a function that queries the
+Reddit API and prints the titles
+of the first 10 hot posts listed
+for a given subreddit.
 """
+import requests
 
 
 def top_ten(subreddit):
     """
     queries the Reddit API
     prints the titles of the first 10 hot posts for subreddit
+    Keyword arguments:
+    subreddit -- the subreddit to check e.g programming.
     """
-    import json
-    import requests
-    subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(
-        subreddit)
-    subreddit_info = requests.get(subreddit_URL,
-                                  headers={"user-agent": "user"},
-                                  allow_redirects=False).json()
-    if "data" not in subreddit_info:
-        print("None")
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"user-agent": "user"}
+    reddit_request = requests.get(url,
+                                  headers=headers,
+                                  allow_redirects=False)
+    if (reddit_request.status_code != 200):
+        print('None')
         return
-    children = subreddit_info.get("data").get("children")
-    for child in children:
-        print(child.get("data").get("title"))
+    reddit_json = reddit_request.json()
+    for reddit in reddit_json['data']['children']:
+        print(reddit.get('data').get('title'))
