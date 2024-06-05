836ec09dd8de929138c4215063b09b908074affd
Iyanu Dayisi <tobiy23@gmail.com>
2024-06-05 08:13:49 +0100

    get all subscribers should work now

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+Write a function that queries the Reddit API
+and returns the number of subscribers
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """Number of Subreddit Subscribers
+    Keyword arguments:
+    subreddit -- the subreddit to check e.g programming.
+    Return: the number of subscribers or 0 if the subreddit doesn't exist
+    """
     try:
         url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
         headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0\
-            (by /u/Large_Alternative_30)"
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) \
+                Gecko/20100101 Firefox/90.0'
         }
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 404:
-            return 0
-        results = response.json().get("data")
-        return results.get("subscribers")
-    except (Exception):
-        print('Not Found')
-        return (0)
+        reddit_request = requests.get(url,
+                                      headers=headers,
+                                      allow_redirects=False)
+        reddit_json = reddit_request.json()
+        return reddit_json.get('data').get('subscribers')
+    except requests.JSONDecodeError as e:
+        return 0
+    except Exception as e:
+        return 0
