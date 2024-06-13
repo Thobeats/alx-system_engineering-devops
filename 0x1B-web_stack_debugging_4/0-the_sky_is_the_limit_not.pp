b4296358587888d2ccb22c9f8950b7988735010c
Iyanu Dayisi <tobiy23@gmail.com>
2024-06-14 00:05:59 +0100

    web debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 exec { 'debugging fix':
   command => "sed -i 's|ULIMIT=\"-n 15\"|ULIMIT=\"-n 4096\"|' /etc/default/nginx",
   path    => '/bin'
-} ~>
-service { 'nginx':
+}
+~> service { 'nginx':
   ensure => running
 }
